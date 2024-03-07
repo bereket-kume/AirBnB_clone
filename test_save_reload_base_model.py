d6744bdb825c95ca2cc58bab79adc45b626cd59b
bereket kume <bereketkume@gmail.com>
2024-03-07 07:43:47 -0800

    done

--- test_save_reload_base_model.py
+++ test_save_reload_base_model.py
@@ -1,16 +1,31 @@
-#!/usr/bin/python3
-from models import storage
+import json
 from models.base_model import BaseModel
 
-all_objs = storage.all()
-print("-- Reloaded objects --")
-for obj_id in all_objs.keys():
-    obj = all_objs[obj_id]
-    print(obj)
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
 
-print("-- Create a new object --")
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-my_model.save()
-print(my_model)
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        json_dict = {}
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(json_dict, f)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                json_dict = json.load(f)
+                for key, value in json_dict.items():
+                    class_name, obj_id = key.split('.')
+                    obj = BaseModel(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
