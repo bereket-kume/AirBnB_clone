3df350213cffc721061d2625ee65496d64f4440c
abdiwak34 <abdiwakamsalu@gmail.com>
2024-03-07 00:02:22 -0800

    done for the task 6

--- console.py
+++ console.py
@@ -2,10 +2,20 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
     def do_help(self, line):
-        print("i'm working")
+        if line == 'quit':
+            print('Quit command to exit the program\n')
+        else:
+            print("\nDocumented commands (type help <topic>):\
+                    \n========================================\
+                    \nEOF  help  quit\n")
     def do_quit(self, line):
         return True
+    def do_EOF(self, line):
+        return True
+    def emptyline(self):
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
