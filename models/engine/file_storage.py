c69db8edbb9bade9149dffdbe1dc328e5c7869d7
bereket kume <bereketkume@gmail.com>
2024-03-07 07:52:38 -0800

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 import json
-from models.base_model import BaseModel
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
@@ -25,8 +25,7 @@ class FileStorage:
                 for key, value in json_dict.items():
                     class_name, obj_id = key.split('.')
                     module_name = class_name.lower()
-                    module = __import__('models.' + module_name, fromlist=[class_name])
-                    class_ = getattr(module, class_name)
+                    class_ = eval(class_name)
                     obj = class_(**value)
                     FileStorage.__objects[key] = obj
         except FileNotFoundError:
