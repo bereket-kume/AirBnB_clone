243e4d30be5695d510db1698ffb54356f6d8f74d
bereket kume <bereketkume@gmail.com>
2024-03-07 09:57:20 -0800

    sone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.base_model import BaseModel
 
 class FileStorage:
     __file_path = "file.json"
