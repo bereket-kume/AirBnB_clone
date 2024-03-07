13e4810a8ee0d9f5c01cafc76d732db026b88254
bereket kume <bereketkume@gmail.com>
2024-03-07 07:33:58 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,59 +1,19 @@
-import json
-from datetime import datetime
+from models import storage
 import uuid
-import models
-class BaseModel:
-    __objects = {}
+from datetime import datetime
 
+class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
+                if key != '__class__':
                     setattr(self, key, value)
-            self.id = kwargs.get("id", str(uuid.uuid4()))
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
-
-    def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict.update({"__class__": self.__class__.__name__})
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
-        return new_dict
-
-    @classmethod
-    def all(cls):
-        return cls.__objects
-
-    @classmethod
-    def new(cls, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        cls.__objects[key] = obj
-
-    @classmethod
-    def save(cls):
-        file_path = cls.__name__ + ".json"
-        obj_dict = {}
-        for key, value in cls.__objects.items():
-            obj_dict[key] = value.to_dict()
-        with open(file_path, mode="w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
-
-    @classmethod
-    def reload(cls):
-        file_path = cls.__name__ + ".json"
-        try:
-            with open(file_path, mode="r", encoding="utf-8") as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
-                    cls.__objects[key] = cls(**value)
-        except FileNotFoundError:
-            pass
+        storage.save()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,33 @@
 import json
-from os.path import exists
-
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        return FileStorage.__objects
 
-        return self.__objects
-    
     def new(self, obj):
-
-        key="{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-
-        serializable_objetcs = {
-            key: obj.to_dict for key, obj in self.__objects.items()
-        }
-
-        with open(self.__file_path, "w") as file:
-            json.dump(serializable_objetcs, file)
+        json_dict = {}
+        for key, value in FileStorage.__objects.items():
+            json_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(json_dict, f)
 
     def reload(self):
-
-        if exists(self.__file_path):
-            with open(self.__file_path, "r") as file:
-                serializable_objects = json.load(file)
-                for key, obj_dict in serializable_objects.items():
-                    class_name, obj_id = key.split(".")
-                    obj = eval(class_name)(**obj_dict)
-                    self.__objects[key] = obj
-                    
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                json_dict = json.load(f)
+                for key, value in json_dict.items():
+                    class_name, obj_id = key.split('.')
+                    module_name = class_name.lower()
+                    module = __import__('models.' + module_name, fromlist=[class_name])
+                    class_ = getattr(module, class_name)
+                    obj = class_(**value)
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
