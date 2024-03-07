8477040ca812157e9000decad9ffe739dc60272d
bereket kume <bereketkume@gmail.com>
2024-03-07 07:59:06 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,23 @@
 import uuid
 from datetime import datetime
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
-                    setattr(self, key, value)
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             from models import storage
             storage.new(self)
+
+
 
     def save(self):
         self.updated_at = datetime.now()
