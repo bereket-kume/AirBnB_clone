43517353f35e81f104eb15794ca3a07e2f8facce
bereket kume <bereketkume@gmail.com>
2024-03-09 03:18:13 -0800

    done

--- tests/test_console.py
+++ tests/test_console.py
@@ -29,45 +29,7 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertTrue(output.startswith("** no instance found **"))
 
    
-    def test_do_all(self):
-        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
-            self.console.onecmd("create BaseModel")
-            self.console.onecmd("create User")
-            self.console.onecmd("create User")
-            self.console.onecmd("create Place")
-            self.console.onecmd("create Place")
-            self.console.onecmd("create Amenity")
-            self.console.onecmd("create Review")
-            self.console.onecmd("do_all")
-            output = mock_stdout.getvalue().strip()
-            self.assertIn("BaseModel", output)
-            self.assertIn("User", output)
-            self.assertIn("Place", output)
-            self.assertIn("Amenity", output)
-            self.assertIn("Review", output)
-
-    def test_do_all_with_class_name(self):
-        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
-            self.console.onecmd("create BaseModel")
-            self.console.onecmd("create User")
-            self.console.onecmd("create User")
-            self.console.onecmd("create Place")
-            self.console.onecmd("create Place")
-            self.console.onecmd("create Amenity")
-            self.console.onecmd("create Review")
-            self.console.onecmd("do_all BaseModel")
-            output = mock_stdout.getvalue().strip()
-            self.assertIn("BaseModel", output)
-            self.assertNotIn("User", output)
-            self.assertNotIn("Place", output)
-            self.assertNotIn("Amenity", output)
-            self.assertNotIn("Review", output)
-
-    def test_do_all_with_invalid_class_name(self):
-        with patch('sys.stdout', new=io.StringIO()) as mock_stdout:
-            self.console.onecmd("do_all InvalidClass")
-            output = mock_stdout.getvalue().strip()
-            self.assertEqual(output, "** class doesn't exist **")
+   
 
     
     
