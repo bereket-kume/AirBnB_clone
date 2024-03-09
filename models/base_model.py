2b1ba9f430782d07cc30bdde3959cdc1eeaab2a4
bereket kume <bereketkume@gmail.com>
2024-03-09 06:39:22 -0800

    done

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, id=None, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -13,12 +13,10 @@ class BaseModel:
                         setattr(
                             self, key, datetime.strptime(
                                 value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    elif key == 'id':
-                        setattr(self, key, value)
                     else:
                         setattr(self, key, value)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = id or str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             from models import storage
